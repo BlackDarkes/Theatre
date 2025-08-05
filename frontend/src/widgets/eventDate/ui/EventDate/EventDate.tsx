138a2b1226847b3fa9bbf5c7d2abff8e8adb4dbc
DaniilGordeev <gordeev.daniil.andreevic@gmail.com>
2025-08-05 23:32:41 +0500

    Замена картинки на div с BAckgroundImage

--- frontend/src/widgets/eventDate/ui/EventDate/EventDate.tsx
+++ frontend/src/widgets/eventDate/ui/EventDate/EventDate.tsx
@@ -11,13 +11,8 @@ export const EventDate = () => {
 
   return (
     <section>
+      <div style={{ backgroundImage: `url(${import.meta.env.VITE_API_URL}${event?.img})`}} className={styles.eventDateImage} aria-label={`Обложка ${event?.title}`} />
       <Container>
-        <img
-          src={`${import.meta.env.VITE_API_URL}${event?.img}`}
-          alt={`Обложка ${event?.title}`}
-          className={styles.eventDateImage}
-        /> 
-        {/* // Изменить на div так как картинка нормально не встаёт*/}
         <div className={styles.eventDateInfo}>
           <ul className={styles.eventDateList}>
             <GenerateDate date={DATE_INFORMATION} />
